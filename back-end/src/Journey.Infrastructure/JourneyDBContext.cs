aaa1eb0df80b32c7248428e0df790c30d5f0a9ba
BrunoMSPais <bpais@outlook.pt>
2024-07-13 02:45:48 +0100

    C# - day 1

---
+++ back-end/src/Journey.Infrastructure/JourneyDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Journey.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Infrastructure
+{
+    public class JourneyDBContext : DbContext
+    {
+        public DbSet<Trip> Trips { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.
+                UseSqlite(
+                "Data Source=C:\\development\\Javascript\\pratica\\rocketseat\\NLW\\NLW-Journey\\back-end\\JourneyDatabase.db"
+                );
+        }
+    }
+}
